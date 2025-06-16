b69e68e29a80b05cdfdc93c0089eb8110d9ff438
Anon-136 <anonjin44@gmail.com>
2025-06-16 17:13:43 +0700

    wip: config anakotmai font

---
+++ apps-client/mobile/assets/fonts/README.md
@@ -0,0 +1,1 @@
+You can get the `anakotmai` font here https://peoplesparty.or.th/font-anakotmai/
--- apps-client/mobile/components/playground.tsx
+++ apps-client/mobile/components/playground.tsx
@@ -1,4 +1,4 @@
-import { View } from 'react-native'
+import { Platform, View } from 'react-native'
 
 import { Button } from '@pple-today/ui/button'
 import { ThemeToggle } from '@pple-today/ui/components/theme-toggle'
@@ -7,12 +7,44 @@ import { H1, H2 } from '@pple-today/ui/typography'
 
 export function Playground() {
   return (
-    <View className="flex flex-col gap-4 w-full">
+    <View className="flex flex-col gap-4 w-full font-sans">
       <View className="flex flex-row items-center justify-between">
         <H1>Playground</H1>
         <ThemeToggle />
       </View>
       <View className="flex flex-col gap-2">
+        <Text>สวัสดี</Text>
+        <Text
+          style={{
+            fontFamily: Platform.select({
+              android: 'NotoSansThaiLooped_400Regular',
+              ios: 'NotoSansThaiLooped-Regular',
+            }),
+          }}
+        >
+          สวัสดี
+        </Text>
+        <Text
+          style={{
+            fontFamily: Platform.select({
+              android: 'Anakotmai',
+              ios: 'Anakotmai',
+            }),
+          }}
+        >
+          สวัสดี
+        </Text>
+        <Text
+          className="text-xl"
+          style={{
+            fontFamily: Platform.select({
+              android: 'Inter_700Bold',
+              ios: 'Inter-Bold',
+            }),
+          }}
+        >
+          Whereas disregard
+        </Text>
         <H2>Button</H2>
         <View className="flex flex-row gap-2 flex-wrap">
           <Button>
