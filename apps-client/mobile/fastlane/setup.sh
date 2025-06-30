dfcdf2911e51c8bd008d11f3daf277724f0ff2e6
Anon-136 <anonjin44@gmail.com>
2025-06-30 18:07:30 +0700

    wip: update beta script to run android only

--- apps-client/mobile/fastlane/setup.sh
+++ apps-client/mobile/fastlane/setup.sh
@@ -10,24 +10,23 @@ source .env
 
 if [ ! -d "./android" ]; then
   echo "Error: The 'android' directory does not exist. Please ensure you have run \`expo prebuild\` first."
-  exit 1
+else
+  cp -r ./fastlane/android/ ./android
+  echo "Fastlane directory copied to android folder."
+
+  if [ -z "$FIREBASE_SERVICE_ACCOUNT_JSON" ]; then
+    echo "Error: FIREBASE_SERVICE_ACCOUNT_JSON environment variable is not set."
+  else
+    echo $FIREBASE_SERVICE_ACCOUNT_JSON | base64 -d > ./android/key.json
+    echo "Firebase service account JSON file created at ./android/key.json."
+  fi
 fi
 if [ ! -d "./ios" ]; then
   echo "Error: The 'ios' directory does not exist. Please ensure you have run \`expo prebuild\` first."
-  exit 1
+else 
+  cp -r ./fastlane/ios/ ./ios
+  echo "Fastlane directory copied to ios folder."
+
+  echo "FASTLANE_USER=$FASTLANE_USER\nFASTLANE_APPLE_APPLICATION_SPECIFIC_PASSWORD=$FASTLANE_APPLE_APPLICATION_SPECIFIC_PASSWORD" > ./ios/fastlane/.env.default
+  echo "Fastlane TestFlight environment variables file created at ./ios/fastlane/.env.default"
 fi
-if [ -z "$FIREBASE_SERVICE_ACCOUNT_JSON" ]; then
-  echo "Error: FIREBASE_SERVICE_ACCOUNT_JSON environment variable is not set."
-  exit 1
-fi
-
-cp -r ./fastlane/android/ ./android
-echo "Fastlane directory copied to android folder."
-
-cp -r ./fastlane/ios/ ./ios
-echo "Fastlane directory copied to ios folder."
-
-echo $FIREBASE_SERVICE_ACCOUNT_JSON | base64 -d > ./android/key.json
-echo "Firebase service account JSON file created at ./android/key.json."
-echo "FASTLANE_USER=$FASTLANE_USER\nFASTLANE_APPLE_APPLICATION_SPECIFIC_PASSWORD=$FASTLANE_APPLE_APPLICATION_SPECIFIC_PASSWORD" > ./ios/fastlane/.env.default
-echo "Fastlane TestFlight environment variables file created at ./ios/fastlane/.env.default"
