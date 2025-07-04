6a34b2145553cc3207d6f07c787ddbc692277ddb
Anon-136 <anonjin44@gmail.com>
2025-07-04 14:32:03 +0700

    chore: setup environment vars

--- apps-client/mobile/fastlane/setup.sh
+++ apps-client/mobile/fastlane/setup.sh
@@ -25,6 +25,9 @@ else
     echo $FIREBASE_SERVICE_ACCOUNT_JSON | base64 -d > ./android/key.json
     echo "Firebase service account JSON file created at ./android/key.json."
   fi
+
+  cp .env ./android/fastlane/.env.default
+  echo "Fastlane environment variables file created at ./android/fastlane/.env.default"
 fi
 if [ ! -d "./ios" ]; then
   echo "Warning: The 'ios' directory does not exist. Please ensure you have run \`expo prebuild\` first."
@@ -33,5 +36,5 @@ else
   echo "Fastlane directory copied to ios folder."
 
   cp .env ./ios/fastlane/.env.default
-  echo "Fastlane TestFlight environment variables file created at ./ios/fastlane/.env.default"
+  echo "Fastlane environment variables file created at ./ios/fastlane/.env.default"
 fi
