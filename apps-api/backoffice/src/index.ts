3e28349298d3310ea5a8b1cafe5b752fa44036bf
miello <partearth1212@gmail.com>
2025-07-05 19:15:22 +0700

    feat: add swagger and prisma schema

--- apps-api/backoffice/src/index.ts
+++ apps-api/backoffice/src/index.ts
@@ -1,9 +1,11 @@
 import node from '@elysiajs/node'
+import { swagger } from '@elysiajs/swagger'
 import Elysia from 'elysia'
 
 import serverEnv from './config/env'
 
 const app = new Elysia({ adapter: node() })
+  .use(swagger())
   .get('/', () => 'Hello, Elysia!')
   .listen(serverEnv.PORT, () => {
     console.log(`Server is running on http://localhost:${serverEnv.PORT}`)
