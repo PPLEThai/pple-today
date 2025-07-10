cc74ca6364203d7d4d052991fa6e653fa8e115cf
miello <partearth1212@gmail.com>
2025-07-10 21:12:40 +0700

    refactor: remove unused code and add checking

--- apps-client/backoffice/eslint.config.js
+++ apps-client/backoffice/eslint.config.js
@@ -1,1 +1,8 @@
-export { default } from '@pple-today/project-config/eslint/react.js'
+import baseConfig from '@pple-today/project-config/eslint/react.js'
+
+export default [
+  ...baseConfig,
+  {
+    ignores: ['node_modules', 'build', '.react-router', 'eslint.config.js'],
+  },
+]
