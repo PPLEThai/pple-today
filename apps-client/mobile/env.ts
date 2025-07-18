b90de5427f88f0653637f1a8b0d606896a361874
Anon-136 <anonjin44@gmail.com>
2025-07-18 13:30:48 +0700

    fix: url

--- apps-client/mobile/env.ts
+++ apps-client/mobile/env.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 const environmentSchema = z.object({
-  EXPO_PUBLIC_OIDC_BASE_URL: z.string().url(),
+  EXPO_PUBLIC_OIDC_BASE_URL: z.url(),
   EXPO_PUBLIC_OIDC_CLIENT_ID: z.string().min(1),
   EXPO_PUBLIC_BACKEND_BASE_URL: z.url(),
 })
