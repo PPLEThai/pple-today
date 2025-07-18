a449aeb2c047021d9e8a53a9ab45fb9d46aa7582
Anon-136 <anonjin44@gmail.com>
2025-07-18 13:27:37 +0700

    fix: update to use env

--- apps-client/mobile/libs/react-query.ts
+++ apps-client/mobile/libs/react-query.ts
@@ -1,8 +1,10 @@
 import { createReactQueryClient } from '@pple-today/api-client'
 
+import { environment } from '@app/env'
+
 const {
   fetchClient,
   reactQueryClient: { useQuery, useMutation, queryOptions, mutationOptions },
-} = createReactQueryClient(process.env.EXPO_PUBLIC_BACKEND_BASE_URL ?? '')
+} = createReactQueryClient(environment.EXPO_PUBLIC_BACKEND_BASE_URL)
 
 export { fetchClient, mutationOptions, queryOptions, useMutation, useQuery }
