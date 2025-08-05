d656456752ea629131adf3e22fb3ab01dce5b097
Anon-136 <anonjin44@gmail.com>
2025-08-06 00:08:25 +0700

    wip: ExpoScrollForwarderView ios

--- packages/expo-scroll-forwarder/ios/ExpoScrollForwarderModule.swift
+++ packages/expo-scroll-forwarder/ios/ExpoScrollForwarderModule.swift
@@ -1,26 +1,13 @@
 import ExpoModulesCore
 
 public class ExpoScrollForwarderModule: Module {
-  // Each module class must implement the definition function. The definition consists of components
-  // that describes the module's functionality and behavior.
-  // See https://docs.expo.dev/modules/module-api for more details about available components.
   public func definition() -> ModuleDefinition {
-    // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
-    // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
-    // The module will be accessible from `requireNativeModule('ExpoScrollForwarder')` in JavaScript.
     Name("ExpoScrollForwarder")
 
-    // Enables the module to be used as a native view. Definition components that are accepted as part of the
-    // view definition: Prop, Events.
     View(ExpoScrollForwarderView.self) {
-      // Defines a setter for the `url` prop.
-      Prop("url") { (view: ExpoScrollForwarderView, url: URL) in
-        if view.webView.url != url {
-          view.webView.load(URLRequest(url: url))
-        }
+      Prop("scrollViewTag") { (view: ExpoScrollForwarderView, prop: Int) in
+        view.scrollViewTag = prop
       }
-
-      Events("onLoad")
     }
   }
 }
