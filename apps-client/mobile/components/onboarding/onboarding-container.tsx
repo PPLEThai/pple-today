c6ee6827d43a3d0405389dab65842be56c61ec18
csap <csirintronsopon8@gmail.com>
2025-08-27 10:17:51 +0700

    style: make accurate to design

--- apps-client/mobile/components/onboarding/onboarding-container.tsx
+++ apps-client/mobile/components/onboarding/onboarding-container.tsx
@@ -8,41 +8,31 @@ import { Text } from '@pple-today/ui/text'
 import { H2 } from '@pple-today/ui/typography'
 import { ArrowLeftIcon } from 'lucide-react-native'
 
-import { OnboardingContext } from '@app/libs/onboarding'
+import { OnboardingContext } from './onboarding'
 
 export function OnboardingContainer({ children }: { children: React.ReactNode }) {
   const { state, dispatch } = React.useContext(OnboardingContext)
 
   return (
-    <View className="size-full flex flex-col">
-      <OnboardingContext.Provider value={{ state, dispatch }}>
-        <View>
-          <View className="flex flex-row items-center gap-4">
-            <Button
-              variant="ghost"
-              className="gap-0"
-              onPress={() => dispatch({ type: 'prev' })}
-              disabled={!state.hasPrev}
-            >
-              <Icon
-                icon={ArrowLeftIcon}
-                size={24}
-                strokeWidth={1}
-                className="text-base-text-high"
-              />
-            </Button>
-            <Progress value={34 * state.activeStepIndex} max={100} className="flex-1" />
-            <Text className="text-base-text-medium">
-              {state.activeStepIndex}/{state.totalSteps}
-            </Text>
-          </View>
-          <View>
-            <OnboardingHeader step={state.activeStepIndex} />
-          </View>
-        </View>
-        {children}
-      </OnboardingContext.Provider>
-    </View>
+    <>
+      <View className="flex flex-row items-center gap-4 pl-2 pr-[22px] py-1">
+        <Button
+          size="icon"
+          variant="ghost"
+          className="gap-0"
+          onPress={() => dispatch({ type: 'prev' })}
+          disabled={!state.hasPrev}
+        >
+          <Icon icon={ArrowLeftIcon} size={24} strokeWidth={1} className="text-base-text-high" />
+        </Button>
+        <Progress value={34 * state.activeStepIndex} max={100} className="flex-1" />
+        <Text className="text-base-text-medium px-2 font-anakotmai-medium">
+          {state.activeStepIndex}/{state.totalSteps}
+        </Text>
+      </View>
+      <OnboardingHeader step={state.activeStepIndex} />
+      {children}
+    </>
   )
 }
 
@@ -70,7 +60,7 @@ function OnboardingHeader({ step }: { step: number }) {
   const { title, description } = getHeaderContent()
 
   return (
-    <View className="flex flex-col py-4">
+    <View className="flex flex-col pt-1 px-6 gap-1">
       <H2 className="text-base-primary-default pt-3 font-anakotmai-medium">{title}</H2>
       <Text className="font-anakotmai-medium text-base-text-medium">{description}</Text>
     </View>
