a07e982840b920c0b34475090d769b31432d53be
teegooodgood <teegooodgood@gmail.com>
2025-09-05 11:46:43 +0700

    add: db migration

---
+++ apps-api/backoffice/prisma/migrations/20250905044340_update_internal_election_models/migration.sql
@@ -0,0 +1,77 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `endDate` on the `Election` table. All the data in the column will be lost.
+  - You are about to drop the column `publicKey` on the `Election` table. All the data in the column will be lost.
+  - You are about to drop the column `startDate` on the `Election` table. All the data in the column will be lost.
+  - You are about to drop the column `ballotId` on the `ElectionEligibleBallot` table. All the data in the column will be lost.
+  - You are about to drop the `ElectionBallot` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `closeVoting` to the `Election` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `openVoting` to the `Election` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `type` to the `Election` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `encryptedBallot` to the `ElectionEligibleBallot` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateEnum
+CREATE TYPE "public"."ElectionType" AS ENUM ('ONSITE', 'ONLINE', 'HYBRID');
+
+-- CreateEnum
+CREATE TYPE "public"."ElectionResultType" AS ENUM ('ONSITE', 'ONLINE');
+
+-- DropForeignKey
+ALTER TABLE "public"."ElectionBallot" DROP CONSTRAINT "ElectionBallot_electionId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "public"."ElectionEligibleBallot" DROP CONSTRAINT "ElectionEligibleBallot_ballotId_fkey";
+
+-- DropIndex
+DROP INDEX "public"."ElectionEligibleBallot_electionId_idx";
+
+-- DropIndex
+DROP INDEX "public"."ElectionEligibleBallot_userId_idx";
+
+-- DropIndex
+DROP INDEX "public"."ElectionEligibleVoter_electionId_idx";
+
+-- AlterTable
+ALTER TABLE "public"."Election" DROP COLUMN "endDate",
+DROP COLUMN "publicKey",
+DROP COLUMN "startDate",
+ADD COLUMN     "closeRegister" TIMESTAMP(3),
+ADD COLUMN     "closeVoting" TIMESTAMP(3) NOT NULL,
+ADD COLUMN     "endResult" TIMESTAMP(3),
+ADD COLUMN     "openRegister" TIMESTAMP(3),
+ADD COLUMN     "openVoting" TIMESTAMP(3) NOT NULL,
+ADD COLUMN     "publishDate" TIMESTAMP(3),
+ADD COLUMN     "startResult" TIMESTAMP(3),
+ADD COLUMN     "type" "public"."ElectionType" NOT NULL;
+
+-- AlterTable
+ALTER TABLE "public"."ElectionCandidate" ADD COLUMN     "number" INTEGER;
+
+-- AlterTable
+ALTER TABLE "public"."ElectionEligibleBallot" DROP COLUMN "ballotId",
+ADD COLUMN     "encryptedBallot" TEXT NOT NULL,
+ADD COLUMN     "faceImageURL" TEXT,
+ADD COLUMN     "location" TEXT;
+
+-- DropTable
+DROP TABLE "public"."ElectionBallot";
+
+-- CreateTable
+CREATE TABLE "public"."ElectionResult" (
+    "id" TEXT NOT NULL,
+    "candidateId" TEXT NOT NULL,
+    "type" "public"."ElectionResultType" NOT NULL,
+    "count" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ElectionResult_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ElectionResult_candidateId_type_key" ON "public"."ElectionResult"("candidateId", "type");
+
+-- AddForeignKey
+ALTER TABLE "public"."ElectionResult" ADD CONSTRAINT "ElectionResult_candidateId_fkey" FOREIGN KEY ("candidateId") REFERENCES "public"."ElectionCandidate"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
