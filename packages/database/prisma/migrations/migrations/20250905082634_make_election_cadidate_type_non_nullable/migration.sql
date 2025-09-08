23bb535834c09d9e5467666cebc4582dd42183fb
teegooodgood <teegooodgood@gmail.com>
2025-09-08 14:04:19 +0700

    fix: move migration file destination

--- packages/database/prisma/migrations/migrations/20250905082634_make_election_cadidate_type_non_nullable/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `type` on table `ElectionEligibleVoter` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "ElectionEligibleVoter" ALTER COLUMN "type" SET NOT NULL;
