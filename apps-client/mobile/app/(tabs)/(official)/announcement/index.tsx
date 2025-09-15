cd616f4d9204faff89e28c7df68e716b8b8f1091
csap <csirintronsopon8@gmail.com>
2025-09-15 16:44:01 +0700

    fix: create new path to handle in feed page

--- apps-client/mobile/app/(tabs)/(official)/announcement/index.tsx
+++ apps-client/mobile/app/(tabs)/(official)/announcement/index.tsx
@@ -110,7 +110,7 @@ function AnnouncementList() {
             <AnnouncementCard
               className="w-full mt-3"
               key={item.id}
-              onPress={() => router.navigate(`/(official)/announcement/${item.id}`)}
+              onPress={() => router.navigate(`./announcement/${item.id}`)}
               id={item.id}
               feedId={item.id}
               title={item.title}
