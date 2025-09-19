07926268a00f45298caa09e61b05f5ea324caa2d
teegooodgood <teegooodgood@gmail.com>
2025-09-19 12:51:00 +0700

    add: clear face image when delete election

--- apps-api/backoffice/src/modules/admin/election/repository.ts
+++ apps-api/backoffice/src/modules/admin/election/repository.ts
@@ -91,8 +91,23 @@ export class AdminElectionRepository {
 
   async deleteElectionById(electionId: string) {
     return fromRepositoryPromise(
-      this.prismaService.election.delete({
-        where: { id: electionId },
+      this.prismaService.$transaction(async (tx) => {
+        const deletedVoteRecords = await tx.electionVoteRecord.findMany({
+          where: {
+            electionId,
+          },
+        })
+
+        tx.election.delete({
+          where: { id: electionId },
+        })
+
+        const faceImagePaths = deletedVoteRecords
+          .map((record) => record.faceImagePath)
+          .filter((path) => path !== null)
+
+        const deleteImageResult = await this.fileService.bulkDeleteFile(faceImagePaths)
+        if (deleteImageResult.isErr()) throw deleteImageResult.error
       })
     )
   }
