1c722c9c858a6b72f0b3a717bed86a094532d82c
Anon-136 <anonjin44@gmail.com>
2025-09-23 22:14:56 +0700

    fix: update font

--- apps-client/mobile/app/(tabs)/(official)/index.tsx
+++ apps-client/mobile/app/(tabs)/(official)/index.tsx
@@ -147,7 +147,7 @@ const InformationSection = () => {
         <View className="w-8 h-8 flex items-center justify-center">
           <Icon icon={InfoIcon} size={32} className="text-base-primary-default" />
         </View>
-        <H2 className="text-2xl font-anakotmai-medium text-base-text-high">ข้อมูลพรรคประชาชน</H2>
+        <H2 className="text-2xl font-heading-semibold text-base-text-high">ข้อมูลพรรคประชาชน</H2>
       </View>
       <View className="mt-4 gap-y-4">
         <View className="flex flex-row gap-x-[12.5px]">
@@ -158,7 +158,7 @@ const InformationSection = () => {
               <View className="flex flex-col mb-3 h-8 w-8 bg-base-secondary-default rounded-lg items-center justify-center">
                 <Personal width={24} height={24} color="white" />
               </View>
-              <Text className="text-base font-anakotmai-medium w-full">บุคลากรของพรรค</Text>
+              <Text className="text-base font-heading-semibold w-full">บุคลากรของพรรค</Text>
             </View>
             <View className="flex-1 justify-end items-end">
               <Icon
@@ -176,7 +176,7 @@ const InformationSection = () => {
               <View className="flex flex-col mb-3 h-8 w-8 bg-base-primary-default rounded-lg items-center justify-center">
                 <PPLEIcon width={20} height={16} color="white" />
               </View>
-              <Text className="text-base font-anakotmai-medium w-full">เกี่ยวกับพรรคประชาชน</Text>
+              <Text className="text-base font-heading-semibold w-full">เกี่ยวกับพรรคประชาชน</Text>
             </View>
             <View className="flex-1 justify-end items-end">
               <Icon
@@ -194,7 +194,7 @@ const InformationSection = () => {
               <View className="flex flex-col mb-3 h-8 w-8 bg-violet-500 rounded-lg items-center justify-center">
                 <ContactMail width={24} height={24} color="white" />
               </View>
-              <Text className="text-base font-anakotmai-medium w-full">ช่องทางการติดต่อ</Text>
+              <Text className="text-base font-heading-semibold w-full">ช่องทางการติดต่อ</Text>
             </View>
             <View className="flex-1 justify-end items-end">
               <Icon
@@ -220,14 +220,18 @@ const InformationSection = () => {
                             strokeWidth={1}
                             className="text-base-primary-default"
                           />
-                          <Text className="text-base-text-high font-noto-medium">สำนักงานใหญ่</Text>
+                          <Text className="text-base-text-high font-body-semibold">
+                            สำนักงานใหญ่
+                          </Text>
                         </View>
                         <View className="gap-0.5">
-                          <Text className="font-noto-light">เลขที่ 167 อาคารอนาคตใหม่ ชั้น 4</Text>
-                          <Text className="font-noto-light">
+                          <Text className="font-body-regular">
+                            เลขที่ 167 อาคารอนาคตใหม่ ชั้น 4
+                          </Text>
+                          <Text className="font-body-regular">
                             รามคำแหง 42 แขวงหัวหมาก เขต บางกะปิ
                           </Text>
-                          <Text className="font-noto-light">กรุงเทพมหานคร 10240</Text>
+                          <Text className="font-body-regular">กรุงเทพมหานคร 10240</Text>
                         </View>
                       </View>
                       <View className="gap-2">
@@ -238,12 +242,12 @@ const InformationSection = () => {
                             strokeWidth={1}
                             className="text-base-primary-default"
                           />
-                          <Text className="text-base-text-high font-noto-medium">เบอร์โทร</Text>
+                          <Text className="text-base-text-high font-body-semibold">เบอร์โทร</Text>
                         </View>
-                        <Text className="font-noto-light items-baseline">
+                        <Text className="font-body-regular items-baseline">
                           <Text
                             onPress={() => Linking.openURL('tel:028215874')}
-                            className="underline font-noto-light"
+                            className="underline font-body-regular"
                           >
                             02-821-5874
                           </Text>{' '}
@@ -258,20 +262,22 @@ const InformationSection = () => {
                             strokeWidth={1}
                             className="text-base-primary-default"
                           />
-                          <Text className="text-base-text-high font-noto-medium">อีเมล</Text>
+                          <Text className="text-base-text-high font-body-semibold">อีเมล</Text>
                         </View>
                         <View className="gap-0.5 mb-10">
                           <Text
-                            className="font-noto-light underline"
+                            className="font-body-regular underline"
                             onPress={() =>
                               Linking.openURL('mailto:office@peoplespartythailand.org')
                             }
                           >
                             office@peoplespartythailand.org
                           </Text>
-                          <Text className="font-noto-light">PeoplesPartyThailand</Text>
-                          <Text className="font-noto-light">@PPLEThailand</Text>
-                          <Text className="font-noto-light">พรรคประชาชน - People&apos;s Party</Text>
+                          <Text className="font-body-regular">PeoplesPartyThailand</Text>
+                          <Text className="font-body-regular">@PPLEThailand</Text>
+                          <Text className="font-body-regular">
+                            พรรคประชาชน - People&apos;s Party
+                          </Text>
                         </View>
                       </View>
                     </View>
@@ -285,7 +291,7 @@ const InformationSection = () => {
               <View className="flex flex-col mb-3 h-8 w-8 bg-blue-500 rounded-lg items-center justify-center">
                 <Icon icon={GlobeIcon} size={22} color="white" />
               </View>
-              <Text className="text-base font-anakotmai-medium w-full">เว็บไซต์ทางการ</Text>
+              <Text className="text-base font-heading-semibold w-full">เว็บไซต์ทางการ</Text>
             </View>
             <View className="flex-1 justify-end items-end">
               <Icon
