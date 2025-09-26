f08b6bfd4989c0e5a96f457394180fb305e241c6
rootEnginear <rootenginear@users.noreply.github.com>
2025-09-26 08:09:20 +0700

    fix: badge color

--- packages/web-ui/src/components/ui/badge.tsx
+++ packages/web-ui/src/components/ui/badge.tsx
@@ -6,7 +6,7 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '../../libs/utils'
 
 const badgeVariants = cva(
-  'inline-flex items-center justify-center rounded-full border px-2 py-0.5 text-xs font-medium w-fit whitespace-nowrap shrink-0 [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden',
+  'inline-flex items-center justify-center rounded-full border px-2 py-0.5 text-xs font-medium w-fit whitespace-nowrap shrink-0 [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-system-danger-default/20 dark:aria-invalid:ring-system-danger-default/40 aria-invalid:border-system-danger-default transition-[color,box-shadow] overflow-hidden',
   {
     variants: {
       variant: {
@@ -14,7 +14,7 @@ const badgeVariants = cva(
         secondary:
           'border-transparent bg-secondary text-secondary-foreground [a&]:hover:bg-secondary/90',
         destructive:
-          'border-transparent bg-destructive text-white [a&]:hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
+          'border-transparent bg-system-danger-default text-white [a&]:hover:bg-system-danger-default/90 focus-visible:ring-system-danger-default/20 dark:focus-visible:ring-system-danger-default/40 dark:bg-system-danger-default/60',
         outline: 'text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground',
         success:
           'border-transparent bg-system-success-default text-base-text-invert [a&]:hover:bg-system-success-default/90',
