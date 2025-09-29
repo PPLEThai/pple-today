ceb67eeaf7afc5c7e3839c440d8f396f82b3c2d8
teegooodgood <teegooodgood@gmail.com>
2025-09-29 13:10:18 +0700

    change: group election candidate endpoints

--- apps-api/backoffice/src/modules/admin/election/index.ts
+++ apps-api/backoffice/src/modules/admin/election/index.ts
@@ -113,145 +113,153 @@ export const AdminElectionController = new Elysia({
       },
     }
   )
-  .get(
-    '/:electionId/candidates',
-    async ({ params, status, adminElectionService }) => {
-      const result = await adminElectionService.listElectionCandidates(params.electionId)
-      if (result.isErr()) return mapErrorCodeToResponse(result.error, status)
+  .group('/:electionId/candidates', (app) =>
+    app
+      .get(
+        '',
+        async ({ params, status, adminElectionService }) => {
+          const result = await adminElectionService.listElectionCandidates(params.electionId)
+          if (result.isErr()) return mapErrorCodeToResponse(result.error, status)
 
-      return status(200, result.value)
-    },
-    {
-      detail: {
-        summary: 'Get Election Candidates',
-        description: 'Get Election Candidates',
-      },
-      requiredLocalUser: true,
-      params: AdminListElectionCandidatesParams,
-      response: {
-        200: AdminListElectionCandidatesResponse,
-        ...createErrorSchema(
-          InternalErrorCode.UNAUTHORIZED,
-          InternalErrorCode.FORBIDDEN,
-          InternalErrorCode.INTERNAL_SERVER_ERROR
-        ),
-      },
-    }
-  )
-  .post(
-    '/upload-url',
-    async ({ body, status, adminElectionService }) => {
-      const result = await adminElectionService.createCandidateProfileUploadURL(body.contentType)
-      if (result.isErr()) return mapErrorCodeToResponse(result.error, status)
+          return status(200, result.value)
+        },
+        {
+          detail: {
+            summary: 'Get Election Candidates',
+            description: 'Get Election Candidates',
+          },
+          requiredLocalUser: true,
+          params: AdminListElectionCandidatesParams,
+          response: {
+            200: AdminListElectionCandidatesResponse,
+            ...createErrorSchema(
+              InternalErrorCode.UNAUTHORIZED,
+              InternalErrorCode.FORBIDDEN,
+              InternalErrorCode.INTERNAL_SERVER_ERROR
+            ),
+          },
+        }
+      )
+      .post(
+        '/upload-url',
+        async ({ body, status, adminElectionService }) => {
+          const result = await adminElectionService.createCandidateProfileUploadURL(
+            body.contentType
+          )
+          if (result.isErr()) return mapErrorCodeToResponse(result.error, status)
 
-      return status(200, result.value)
-    },
-    {
-      detail: {
-        summary: 'Get Candidate Profile Upload URL',
-        description: 'Fetch the signed URL to upload candidate profile',
-      },
-      requiredLocalUser: true,
-      body: AdminCreateCandidateProfileUploadURLBody,
-      response: {
-        200: AdminCreateCandidateProfileUploadURLResponse,
-        ...createErrorSchema(
-          InternalErrorCode.UNAUTHORIZED,
-          InternalErrorCode.FILE_UNSUPPORTED_MIME_TYPE,
-          InternalErrorCode.FILE_CREATE_SIGNED_URL_ERROR,
-          InternalErrorCode.INTERNAL_SERVER_ERROR
-        ),
-      },
-    }
-  )
-  .post(
-    '/:electionId/candidates',
-    async ({ params, body, status, adminElectionService }) => {
-      const result = await adminElectionService.createElectionCandidate(params.electionId, body)
-      if (result.isErr()) return mapErrorCodeToResponse(result.error, status)
+          return status(200, result.value)
+        },
+        {
+          detail: {
+            summary: 'Get Candidate Profile Upload URL',
+            description: 'Fetch the signed URL to upload candidate profile',
+          },
+          requiredLocalUser: true,
+          body: AdminCreateCandidateProfileUploadURLBody,
+          response: {
+            200: AdminCreateCandidateProfileUploadURLResponse,
+            ...createErrorSchema(
+              InternalErrorCode.UNAUTHORIZED,
+              InternalErrorCode.FILE_UNSUPPORTED_MIME_TYPE,
+              InternalErrorCode.FILE_CREATE_SIGNED_URL_ERROR,
+              InternalErrorCode.INTERNAL_SERVER_ERROR
+            ),
+          },
+        }
+      )
+      .post(
+        '',
+        async ({ params, body, status, adminElectionService }) => {
+          const result = await adminElectionService.createElectionCandidate(params.electionId, body)
+          if (result.isErr()) return mapErrorCodeToResponse(result.error, status)
 
-      return status(201, result.value)
-    },
-    {
-      detail: {
-        summary: 'Create Election Candidate',
-        description: 'Create Election Candidate',
-      },
-      requiredLocalUser: true,
-      params: AdminCreateElectionCandidateParams,
-      body: AdminCreateElectionCandidateBody,
-      response: {
-        201: AdminCreateElectionCandidateResponse,
-        ...createErrorSchema(
-          InternalErrorCode.UNAUTHORIZED,
-          InternalErrorCode.FORBIDDEN,
-          InternalErrorCode.ELECTION_NOT_FOUND,
-          InternalErrorCode.FILE_MOVE_ERROR,
-          InternalErrorCode.FILE_ROLLBACK_FAILED,
-          InternalErrorCode.FILE_CHANGE_PERMISSION_ERROR,
-          InternalErrorCode.INTERNAL_SERVER_ERROR
-        ),
-      },
-    }
-  )
-  .put(
-    '/:electionId/candidates/:candidateId',
-    async ({ params, body, status, adminElectionService }) => {
-      const result = await adminElectionService.updateElectionCandidate(params.candidateId, body)
-      if (result.isErr()) return mapErrorCodeToResponse(result.error, status)
+          return status(201, result.value)
+        },
+        {
+          detail: {
+            summary: 'Create Election Candidate',
+            description: 'Create Election Candidate',
+          },
+          requiredLocalUser: true,
+          params: AdminCreateElectionCandidateParams,
+          body: AdminCreateElectionCandidateBody,
+          response: {
+            201: AdminCreateElectionCandidateResponse,
+            ...createErrorSchema(
+              InternalErrorCode.UNAUTHORIZED,
+              InternalErrorCode.FORBIDDEN,
+              InternalErrorCode.ELECTION_NOT_FOUND,
+              InternalErrorCode.FILE_MOVE_ERROR,
+              InternalErrorCode.FILE_ROLLBACK_FAILED,
+              InternalErrorCode.FILE_CHANGE_PERMISSION_ERROR,
+              InternalErrorCode.INTERNAL_SERVER_ERROR
+            ),
+          },
+        }
+      )
+      .put(
+        '/:candidateId',
+        async ({ params, body, status, adminElectionService }) => {
+          const result = await adminElectionService.updateElectionCandidate(
+            params.candidateId,
+            body
+          )
+          if (result.isErr()) return mapErrorCodeToResponse(result.error, status)
 
-      return status(200, result.value)
-    },
-    {
-      detail: {
-        summary: 'Update Election Candidate',
-        description: 'Update Election Candidate',
-      },
-      requiredLocalUser: true,
-      params: AdminUpdateElectionCandidateParams,
-      body: AdminCreateElectionCandidateBody,
-      response: {
-        200: AdminCreateElectionCandidateResponse,
-        ...createErrorSchema(
-          InternalErrorCode.UNAUTHORIZED,
-          InternalErrorCode.FORBIDDEN,
-          InternalErrorCode.ELECTION_NOT_FOUND,
-          InternalErrorCode.ELECTION_CANDIDATE_NOT_FOUND,
-          InternalErrorCode.FILE_MOVE_ERROR,
-          InternalErrorCode.FILE_ROLLBACK_FAILED,
-          InternalErrorCode.FILE_CHANGE_PERMISSION_ERROR,
-          InternalErrorCode.INTERNAL_SERVER_ERROR
-        ),
-      },
-    }
-  )
-  .delete(
-    '/:electionId/candidates/:candidateId',
-    async ({ params, status, adminElectionService }) => {
-      const result = await adminElectionService.deleteElectionCandidate(params.candidateId)
-      if (result.isErr()) return mapErrorCodeToResponse(result.error, status)
+          return status(200, result.value)
+        },
+        {
+          detail: {
+            summary: 'Update Election Candidate',
+            description: 'Update Election Candidate',
+          },
+          requiredLocalUser: true,
+          params: AdminUpdateElectionCandidateParams,
+          body: AdminCreateElectionCandidateBody,
+          response: {
+            200: AdminCreateElectionCandidateResponse,
+            ...createErrorSchema(
+              InternalErrorCode.UNAUTHORIZED,
+              InternalErrorCode.FORBIDDEN,
+              InternalErrorCode.ELECTION_NOT_FOUND,
+              InternalErrorCode.ELECTION_CANDIDATE_NOT_FOUND,
+              InternalErrorCode.FILE_MOVE_ERROR,
+              InternalErrorCode.FILE_ROLLBACK_FAILED,
+              InternalErrorCode.FILE_CHANGE_PERMISSION_ERROR,
+              InternalErrorCode.INTERNAL_SERVER_ERROR
+            ),
+          },
+        }
+      )
+      .delete(
+        '/:candidateId',
+        async ({ params, status, adminElectionService }) => {
+          const result = await adminElectionService.deleteElectionCandidate(params.candidateId)
+          if (result.isErr()) return mapErrorCodeToResponse(result.error, status)
 
-      return status(200, { message: 'Delete Election Candidate Successfully' })
-    },
-    {
-      detail: {
-        summary: 'Delete Election Candidate',
-        description: 'Delete Election Candidate',
-      },
-      requiredLocalUser: true,
-      params: AdminDeleteElectionCandidateParams,
-      response: {
-        200: AdminDeleteElectionCandidateResponse,
-        ...createErrorSchema(
-          InternalErrorCode.UNAUTHORIZED,
-          InternalErrorCode.FORBIDDEN,
-          InternalErrorCode.ELECTION_NOT_FOUND,
-          InternalErrorCode.ELECTION_CANDIDATE_NOT_FOUND,
-          InternalErrorCode.FILE_MOVE_ERROR,
-          InternalErrorCode.FILE_ROLLBACK_FAILED,
-          InternalErrorCode.FILE_CHANGE_PERMISSION_ERROR,
-          InternalErrorCode.INTERNAL_SERVER_ERROR
-        ),
-      },
-    }
+          return status(200, { message: 'Delete Election Candidate Successfully' })
+        },
+        {
+          detail: {
+            summary: 'Delete Election Candidate',
+            description: 'Delete Election Candidate',
+          },
+          requiredLocalUser: true,
+          params: AdminDeleteElectionCandidateParams,
+          response: {
+            200: AdminDeleteElectionCandidateResponse,
+            ...createErrorSchema(
+              InternalErrorCode.UNAUTHORIZED,
+              InternalErrorCode.FORBIDDEN,
+              InternalErrorCode.ELECTION_NOT_FOUND,
+              InternalErrorCode.ELECTION_CANDIDATE_NOT_FOUND,
+              InternalErrorCode.FILE_MOVE_ERROR,
+              InternalErrorCode.FILE_ROLLBACK_FAILED,
+              InternalErrorCode.FILE_CHANGE_PERMISSION_ERROR,
+              InternalErrorCode.INTERNAL_SERVER_ERROR
+            ),
+          },
+        }
+      )
   )
