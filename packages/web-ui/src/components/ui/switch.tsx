d26f009799df786e4faafa36640c4134ab3d3068
rootEnginear <rootenginear@users.noreply.github.com>
2025-09-29 17:24:45 +0700

    fix: busy switch

--- packages/web-ui/src/components/ui/switch.tsx
+++ packages/web-ui/src/components/ui/switch.tsx
@@ -1,17 +1,23 @@
 import * as React from 'react'
 
 import * as SwitchPrimitive from '@radix-ui/react-switch'
+import { LoaderCircle } from 'lucide-react'
 
 import { cn } from '../../libs/utils'
 
-function Switch({ className, ...props }: React.ComponentProps<typeof SwitchPrimitive.Root>) {
-  return (
+function Switch({
+  className,
+  isPending,
+  ...props
+}: { isPending?: boolean } & React.ComponentProps<typeof SwitchPrimitive.Root>) {
+  const SwitchBase = (
     <SwitchPrimitive.Root
       data-slot="switch"
       className={cn(
         'peer data-[state=checked]:bg-primary data-[state=unchecked]:bg-input focus-visible:border-ring focus-visible:ring-ring/50 dark:data-[state=unchecked]:bg-input/80 inline-flex h-[1.5rem] w-11 shrink-0 items-center rounded-full border border-transparent shadow-sm transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50',
         className
       )}
+      aria-busy={isPending ?? props['aria-busy']}
       {...props}
     >
       <SwitchPrimitive.Thumb
@@ -21,6 +27,17 @@ function Switch({ className, ...props }: React.ComponentProps<typeof SwitchPrimi
         )}
       />
     </SwitchPrimitive.Root>
+  )
+
+  return isPending ? (
+    <div className="relative flex">
+      {SwitchBase}
+      <div className="absolute left-[52px] top-1/2 -translate-y-1/2">
+        <LoaderCircle className="animate-spin size-5 stroke-base-secondary-light" />
+      </div>
+    </div>
+  ) : (
+    SwitchBase
   )
 }
 
