214dbd6f7b3b740a347766dc271992b25d3aa85a
miello <partearth1212@gmail.com>
2025-10-01 16:19:48 +0700

    feat: update db types

--- packages/database/__generated__/prisma/sql/$DbEnums.d.ts
+++ packages/database/__generated__/prisma/sql/$DbEnums.d.ts
@@ -3,11 +3,14 @@ export interface $DbEnums {}
 
 export namespace $DbEnums {
   type AnnouncementType = "OFFICIAL" | "PARTY_COMMUNICATE" | "INTERNAL"
+  type AuditLogAction = "CREATE" | "UPDATE" | "DELETE"
+  type EntityType = "POST" | "POLL" | "ANNOUNCEMENT" | "HASHTAG" | "TOPIC" | "BANNER" | "ELECTION" | "FACEBOOK_PAGE" | "USER"
   type BannerStatusType = "PUBLISH" | "DRAFT"
   type BannerNavigationType = "IN_APP_NAVIGATION" | "EXTERNAL_BROWSER" | "MINI_APP"
   type ElectionType = "ONSITE" | "ONLINE" | "HYBRID"
   type EligibleVoterType = "ONSITE" | "ONLINE"
   type ElectionResultType = "ONSITE" | "ONLINE"
+  type ElectionMode = "SECURE" | "FLEXIBLE"
   type FeedItemType = "POST" | "POLL" | "ANNOUNCEMENT"
   type FeedItemReactionType = "UP_VOTE" | "DOWN_VOTE"
   type PollType = "SINGLE_CHOICE" | "MULTIPLE_CHOICE"
