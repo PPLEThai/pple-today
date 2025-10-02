9abbb1f70d83b3331239c42e768152579b64201c
miello <partearth1212@gmail.com>
2025-10-02 16:08:15 +0700

    feat: add status to recommendation function

--- packages/database/prisma/functions/recommends_topic/get_candidate_topic_by_interaction.sql
+++ packages/database/prisma/functions/recommends_topic/get_candidate_topic_by_interaction.sql
@@ -15,21 +15,23 @@ BEGIN
     latest_user_interaction AS (
         SELECT 
             "FeedItemReaction"."feedItemId" AS feed_item_id, 
-            CASE
-                WHEN "FeedItemReaction"."type" = 'UP_VOTE' THEN 3
-                WHEN "FeedItemReaction"."type" = 'DOWN_VOTE' THEN 1
-                ELSE 0
-            END AS score
+            SUM(
+	            CASE
+	                WHEN "FeedItemReaction"."type" = 'UP_VOTE' THEN 3
+	                WHEN "FeedItemReaction"."type" = 'DOWN_VOTE' THEN 1
+	                ELSE 0
+	            END
+            ) AS score
         FROM "FeedItemReaction"
         WHERE
             "FeedItemReaction"."userId" = _id
+        GROUP BY feed_item_id
         UNION ALL
-        SELECT 
-          "FeedItemComment"."feedItemId" AS feed_item_id, 
-          1 AS score
+        SELECT "FeedItemComment"."feedItemId" AS feed_item_id, COUNT(*) AS score
         FROM "FeedItemComment"
         WHERE
             "FeedItemComment"."userId" = _id
+        GROUP BY feed_item_id
     ),
     direct_topic_from_interaction AS (
       SELECT 
@@ -37,6 +39,7 @@ BEGIN
         SUM(latest_user_interaction.score) AS score
       FROM 
         latest_user_interaction
+        INNER JOIN "Announcement" a ON a."feedItemId" = latest_user_interaction."feed_item_id" AND a."status" = 'PUBLISHED'
         INNER JOIN "AnnouncementTopic" ant ON ant."announcementId" = latest_user_interaction."feed_item_id"
         LEFT JOIN current_user_follows uft ON uft."topic_id" = ant."topicId"
       WHERE uft."topic_id" IS NULL
@@ -47,6 +50,7 @@ BEGIN
         SUM(latest_user_interaction.score) AS score
       FROM
         latest_user_interaction
+        INNER JOIN "Poll" poll_item ON poll_item."feedItemId" = latest_user_interaction."feed_item_id" AND poll_item."status" = 'PUBLISHED'
         INNER JOIN "PollTopic" poll ON poll."pollId" = latest_user_interaction."feed_item_id"
         LEFT JOIN current_user_follows uft ON uft."topic_id" = poll."topicId"
       WHERE uft."topic_id" IS NULL
@@ -58,6 +62,7 @@ BEGIN
         SUM(latest_user_interaction.score) AS score
       FROM 
         latest_user_interaction
+        INNER JOIN "Post" post ON post."feedItemId" = latest_user_interaction."feed_item_id" AND post."status" = 'PUBLISHED'
         INNER JOIN "PostHashTag" pht ON pht."postId" = latest_user_interaction."feed_item_id"
         INNER JOIN "HashTagInTopic" hitt ON hitt."hashTagId" = pht."hashTagId"
         LEFT JOIN current_user_follows uft ON uft."topic_id" = hitt."topicId"
@@ -84,7 +89,7 @@ BEGIN
   FROM 
     candidate_topic
     INNER JOIN "Topic" t ON candidate_topic.topic_id = t."id"
-  WHERE t."status" = 'PUBLISH'
+  WHERE t."status" = 'PUBLISHED'
   GROUP BY candidate_topic.topic_id
   ORDER BY score DESC
   LIMIT 10;
