5536078489d4bc264980e131b4bf37dc048965a0
miello <partearth1212@gmail.com>
2025-10-16 17:33:48 +0700

    feat: add endpoint for exchange token mini-app

--- apps-api/backoffice/src/modules/auth/models.ts
+++ apps-api/backoffice/src/modules/auth/models.ts
@@ -29,3 +29,35 @@ export const GetAuthMeResponse = t.Object({
 
 export type GetAuthMeHeaders = Static<typeof GetAuthMeHeaders>
 export type GetAuthMeResponse = Static<typeof GetAuthMeResponse>
+
+export const CreateMiniAppTokenParams = t.Object({
+  appId: t.String({ description: 'The ID of the mini app' }),
+})
+export type CreateMiniAppTokenParams = Static<typeof CreateMiniAppTokenParams>
+
+export const CreateMiniAppTokenResponse = t.Object({
+  accessToken: t.String({ description: 'Access token for the mini app' }),
+  expiresIn: t.Number({ description: 'Expiration time of the access token in seconds' }),
+  id_token: t.String({ description: 'ID token of the authorized user' }),
+  token_type: t.String({ description: 'Type of the token, e.g., Bearer' }),
+})
+export type CreateMiniAppTokenResponse = Static<typeof CreateMiniAppTokenResponse>
+
+export const GenerateMiniAppTokenResponse = t.Object({
+  access_token: t.String({ description: 'An access_token as JWT or opaque token' }),
+  expires_in: t.Number({
+    description: 'Number of second until the expiration of the access_token',
+  }),
+  id_token: t.String({ description: 'An id_token of the authorized user' }),
+  scope: t.String({
+    description:
+      'Scopes of the access_token. These might differ from the provided scope parameter.',
+  }),
+  refresh_token: t.String({
+    description: 'An opaque token. Only returned if offline_access scope was requested',
+  }),
+  token_type: t.Literal('Bearer', {
+    description: 'Type of the access_token. Value is always Bearer',
+  }),
+})
+export type GenerateMiniAppTokenResponse = Static<typeof GenerateMiniAppTokenResponse>
--- apps-api/backoffice/src/modules/auth/services.ts
+++ apps-api/backoffice/src/modules/auth/services.ts
@@ -2,18 +2,69 @@ import { InternalErrorCode } from '@pple-today/api-common/dtos'
 import { IntrospectAccessTokenResult } from '@pple-today/api-common/dtos'
 import { FileService } from '@pple-today/api-common/services'
 import { mapRepositoryError } from '@pple-today/api-common/utils'
+import { Check } from '@sinclair/typebox/value'
 import Elysia from 'elysia'
 import { ok } from 'neverthrow'
 
+import { GenerateMiniAppTokenResponse } from './models'
 import { AuthRepository, AuthRepositoryPlugin } from './repository'
 
+import { ConfigServicePlugin } from '../../plugins/config'
 import { FileServicePlugin } from '../../plugins/file'
+import { generateJwtToken } from '../../utils/jwt'
 
 export class AuthService {
   constructor(
     private authRepository: AuthRepository,
-    private fileService: FileService
+    private fileService: FileService,
+    private oidcConfig: {
+      oidcClientId: string
+      oidcUrl: string
+      oidcPrivateJwtKey: string
+      oidcKeyId: string
+    }
   ) {}
+
+  async generateMiniAppToken(clientId: string, token: string) {
+    const jwtToken = generateJwtToken(this.oidcConfig)
+
+    const httpHeaders = { 'Content-Type': 'application/x-www-form-urlencoded' }
+    const data = {
+      client_assertion_type: 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
+      client_assertion: jwtToken,
+      token: token,
+    }
+
+    const response = await fetch(`${this.oidcConfig.oidcUrl}/mini-app/${clientId}`, {
+      method: 'POST',
+      headers: httpHeaders,
+      body: new URLSearchParams(data),
+    })
+
+    if (!response.ok) {
+      return mapRepositoryError({
+        code: InternalErrorCode.INTERNAL_SERVER_ERROR,
+        message: 'An error occurred while generating the mini app token',
+      })
+    }
+
+    const body = await response.json()
+    const miniAppToken = Check(GenerateMiniAppTokenResponse, body)
+
+    if (!miniAppToken) {
+      return mapRepositoryError({
+        code: InternalErrorCode.INTERNAL_SERVER_ERROR,
+        message: 'An error occurred while generating the mini app token',
+      })
+    }
+
+    return ok({
+      accessToken: body.access_token,
+      expiresIn: body.expires_in,
+      id_token: body.id_token,
+      token_type: body.token_type,
+    })
+  }
 
   async getUserById(id: string) {
     const user = await this.authRepository.getUserById(id)
@@ -57,7 +108,12 @@ export class AuthService {
 }
 
 export const AuthServicePlugin = new Elysia({ name: 'AuthService' })
-  .use([AuthRepositoryPlugin, FileServicePlugin])
-  .decorate(({ authRepository, fileService }) => ({
-    authService: new AuthService(authRepository, fileService),
+  .use([AuthRepositoryPlugin, FileServicePlugin, ConfigServicePlugin])
+  .decorate(({ authRepository, fileService, configService }) => ({
+    authService: new AuthService(authRepository, fileService, {
+      oidcClientId: configService.get('OIDC_CLIENT_ID'),
+      oidcUrl: configService.get('OIDC_URL'),
+      oidcPrivateJwtKey: configService.get('OIDC_PRIVATE_JWT_KEY'),
+      oidcKeyId: configService.get('OIDC_KEY_ID'),
+    }),
   }))
