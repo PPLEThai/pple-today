dc1a76d3b9965ba1ed561b0642c76dcf8f65ca86
Anon-136 <anonjin44@gmail.com>
2025-10-16 11:07:23 +0700

    fix: profile page and edit

--- apps-client/mobile/app/(tabs)/(profile)/_layout.tsx
+++ apps-client/mobile/app/(tabs)/(profile)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Stack } from 'expo-router'
 
 export default function Layout() {
-  return <Stack initialRouteName="index" screenOptions={{ headerShown: false }} />
+  return <Stack initialRouteName="profile/index" screenOptions={{ headerShown: false }} />
 }
