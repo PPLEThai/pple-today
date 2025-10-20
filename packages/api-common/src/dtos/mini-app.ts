b3eca416ccba8f2185227832246fffc74a369fc1
miello <partearth1212@gmail.com>
2025-10-20 16:15:54 +0700

    fix: missing icon url from dtos

--- packages/api-common/src/dtos/mini-app.ts
+++ packages/api-common/src/dtos/mini-app.ts
@@ -3,6 +3,11 @@ import { Static, t } from 'elysia'
 export const MiniApp = t.Object({
   id: t.String({ description: 'ID of the mini app' }),
   name: t.String({ description: 'Name of the mini app' }),
+  iconUrl: t.Nullable(
+    t.String({
+      description: 'Icon URL of the mini app',
+    })
+  ),
   url: t.String({
     description: 'Client URL of the mini app',
     format: 'uri',
