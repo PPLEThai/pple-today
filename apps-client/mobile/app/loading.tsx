9dca6d04415be88315b3e3cbcd4f6c097276992e
Anon-136 <anonjin44@gmail.com>
2025-10-21 12:57:27 +0700

    fix: dismiss loading screen instead of navigate

--- apps-client/mobile/app/loading.tsx
+++ apps-client/mobile/app/loading.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import { View } from 'react-native'
+import { BackHandler, View } from 'react-native'
 
 import { useNavigation } from 'expo-router'
 
@@ -9,13 +9,11 @@ export default function LoadingPage() {
   const navigation = useNavigation()
 
   useEffect(() => {
-    // block back button navigation
-    const listener = navigation.addListener('beforeRemove', (e) => {
-      e.preventDefault()
+    // block back button on android
+    const backHandler = BackHandler.addEventListener('hardwareBackPress', () => {
+      return true
     })
-    return () => {
-      navigation.removeListener('beforeRemove', listener)
-    }
+    return () => backHandler.remove()
   }, [navigation])
   return (
     <View className="flex-1 flex flex-col items-center justify-center">
