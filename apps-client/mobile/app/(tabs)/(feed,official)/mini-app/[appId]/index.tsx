8bef1b0516425b1e5633e1d0e7243f22a4add645
miello <partearth1212@gmail.com>
2025-10-21 17:55:35 +0700

    refactor: add back button in navbar

--- apps-client/mobile/app/(tabs)/(feed,official)/mini-app/[appId]/index.tsx
+++ apps-client/mobile/app/(tabs)/(feed,official)/mini-app/[appId]/index.tsx
@@ -1,8 +1,12 @@
 import { useEffect } from 'react'
+import { View } from 'react-native'
 import { WebView } from 'react-native-webview'
 
+import { Button } from '@pple-today/ui/button'
+import { Icon } from '@pple-today/ui/icon'
 import Constants from 'expo-constants'
 import { useLocalSearchParams, useRouter } from 'expo-router'
+import { ArrowLeftIcon } from 'lucide-react-native'
 
 import NotFound from '@app/app/+not-found'
 import LoadingPage from '@app/app/loading'
@@ -27,20 +31,32 @@ const MiniAppWebView = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [electionId, router])
 
-  if (tokenExchangeMiniAppResult.isPending) {
-    return <LoadingPage />
-  }
-
-  if (!tokenExchangeMiniAppResult.isSuccess) {
-    return <NotFound />
-  }
-
   return (
-    <WebView
-      userAgent={`PPLETodayApp/${Constants.expoConfig?.version ?? 'local'}`}
-      source={{ uri: tokenExchangeMiniAppResult.data.url }}
-      startInLoadingState={true}
-    />
+    <View className="flex-1 flex-col bg-base-bg-default">
+      <View className="pt-safe-offset-4 pb-2 px-4 bg-base-bg-white">
+        <Button
+          variant="outline-primary"
+          size="icon"
+          onPress={() => router.back()}
+          aria-label="Go back"
+        >
+          <Icon icon={ArrowLeftIcon} size={24} />
+        </Button>
+      </View>
+      <View className="flex-1">
+        {tokenExchangeMiniAppResult.isPending ? (
+          <LoadingPage />
+        ) : !tokenExchangeMiniAppResult.isSuccess ? (
+          <NotFound />
+        ) : (
+          <WebView
+            className="border-8 border-red-600"
+            userAgent={`PPLETodayApp/${Constants.expoConfig?.version ?? 'local'}`}
+            source={{ uri: tokenExchangeMiniAppResult.data.url }}
+          />
+        )}
+      </View>
+    </View>
   )
 }
 
