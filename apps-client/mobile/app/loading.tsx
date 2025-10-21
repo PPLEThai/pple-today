5af19100f275d622efa1d9d53401389894f827d0
Anon-136 <anonjin44@gmail.com>
2025-10-21 16:21:42 +0700

    chore: remove navigation

--- apps-client/mobile/app/loading.tsx
+++ apps-client/mobile/app/loading.tsx
@@ -1,20 +1,16 @@
 import { useEffect } from 'react'
 import { BackHandler, View } from 'react-native'
 
-import { useNavigation } from 'expo-router'
-
 import { Spinner } from '@app/components/spinner'
 
 export default function LoadingPage() {
-  const navigation = useNavigation()
-
   useEffect(() => {
     // block back button on android
     const backHandler = BackHandler.addEventListener('hardwareBackPress', () => {
       return true
     })
     return () => backHandler.remove()
-  }, [navigation])
+  }, [])
   return (
     <View className="flex-1 flex flex-col items-center justify-center">
       <Spinner />
