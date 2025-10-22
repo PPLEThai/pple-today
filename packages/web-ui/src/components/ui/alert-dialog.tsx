5dd2a9bd863fb78b2d4cffa971efb1df017040f8
rootEnginear <rootenginear@users.noreply.github.com>
2025-10-23 04:03:53 +0700

    fix: alert-dialog wrong cn

--- packages/web-ui/src/components/ui/alert-dialog.tsx
+++ packages/web-ui/src/components/ui/alert-dialog.tsx
@@ -3,9 +3,10 @@
 import * as React from 'react'
 
 import * as AlertDialogPrimitive from '@radix-ui/react-alert-dialog'
-import { cn } from 'libs/utils'
 
 import { buttonVariants } from './button'
+
+import { cn } from '../../libs/utils'
 
 function AlertDialog({ ...props }: React.ComponentProps<typeof AlertDialogPrimitive.Root>) {
   return <AlertDialogPrimitive.Root data-slot="alert-dialog" {...props} />
