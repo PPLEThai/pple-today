c2f846d1765ac01538711d0b3ec5257553ed0345
teegooodgood <teegooodgood@gmail.com>
2025-10-30 20:47:52 +0700

    feat: election page

---
+++ apps-client/backoffice/app/routes/activity.internal-election._index/route.tsx
@@ -0,0 +1,37 @@
+import { NavLink } from 'react-router'
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@pple-today/web-ui/breadcrumb'
+import { Typography } from '@pple-today/web-ui/typography'
+
+export function meta() {
+  return [{ title: 'Internal Election' }]
+}
+
+export default function InternalElectionPage() {
+  return (
+    <div className="px-6 pb-6 space-y-2">
+      <Breadcrumb className="pt-4 pb-2">
+        <BreadcrumbList>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <NavLink to="/activity">Activity</NavLink>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Internal Election</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <Typography variant="h1">Internal Election Management</Typography>
+    </div>
+  )
+}
