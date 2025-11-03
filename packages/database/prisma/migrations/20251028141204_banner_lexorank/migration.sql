27063f85bc59879f5bd8b43259264b53c2840f9b
rootEnginear <rootenginear@users.noreply.github.com>
2025-11-03 18:28:08 +0700

    fix: comments

--- packages/database/prisma/migrations/20251028141204_banner_lexorank/migration.sql
+++ packages/database/prisma/migrations/20251028141204_banner_lexorank/migration.sql
@@ -1,3 +1,11 @@
 -- AlterTable
+BEGIN;
+
+-- Step 1: change type from number to text
 ALTER TABLE "public"."Banner" ALTER COLUMN "order" DROP DEFAULT,
-ALTER COLUMN "order" SET DATA TYPE TEXT;
+ALTER COLUMN "order" TYPE TEXT USING "order"::TEXT;
+
+-- Step 2: transform old numeric values to lexorank format
+UPDATE "public"."Banner" SET "order" = '0|' || LPAD("order", 6, '0') || ':';
+
+COMMIT;
