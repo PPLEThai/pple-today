45fa585c4e1e819a900ea22c5d728b7ca9b39ac0
Anon-136 <anonjin44@gmail.com>
2025-11-04 10:26:26 +0700

    fix: add eslint rules for router

--- apps-client/backoffice/eslint.config.js
+++ apps-client/backoffice/eslint.config.js
@@ -1,7 +1,9 @@
 import baseConfig from '@pple-today/project-config/eslint/react.js'
+import pluginRouter from '@tanstack/eslint-plugin-router'
 
 export default [
   ...baseConfig,
+  ...pluginRouter.configs['flat/recommended'],
   {
     ignores: ['node_modules', 'build', 'routeTree.gen.ts', 'eslint.config.js'],
   },
