23bc2a4ca97560ecf27f412b93b4b8cb11d83993
Anon-136 <anonjin44@gmail.com>
2025-11-06 16:02:50 +0700

    fix: add head content to root route

--- apps-client/backoffice/app/routes/__root.tsx
+++ apps-client/backoffice/app/routes/__root.tsx
@@ -2,12 +2,17 @@ import '../app.css'
 
 import React from 'react'
 
-import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
+import { createRootRouteWithContext, HeadContent, Outlet } from '@tanstack/react-router'
 
 import { RouterContext } from '~/main'
 
 export const Route = createRootRouteWithContext<RouterContext>()({ component: RootLayout })
 
 function RootLayout() {
-  return <Outlet />
+  return (
+    <>
+      <HeadContent />
+      <Outlet />
+    </>
+  )
 }
