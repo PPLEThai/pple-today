17330c07e1ef96bafa704b9de73451b48e47fae7
miello <partearth1212@gmail.com>
2025-11-20 18:09:52 +0700

    refactor: optional sidebar user

--- apps-client/backoffice/components/SidebarUser.tsx
+++ apps-client/backoffice/components/SidebarUser.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren } from 'react'
 interface SidebarUserProps {
   src: string
   title: string
-  subtitle: string
+  subtitle?: string
 }
 
 export const SidebarUser = ({
@@ -17,7 +17,7 @@ export const SidebarUser = ({
       <img className="size-8 shrink-0 rounded-lg" src={src} alt="" />
       <div className="flex-1 min-w-0 flex flex-col gap-0.5">
         <div className="text-sm font-medium text-base-text-high">{title}</div>
-        <div className="text-xs font-light text-base-text-medium">{subtitle}</div>
+        {subtitle && <div className="text-xs font-light text-base-text-medium">{subtitle}</div>}
       </div>
       {children}
     </div>
