b407c7d66ebdbf3791af79599b9608814f057f64
Anon-136 <anonjin44@gmail.com>
2025-11-21 12:00:16 +0700

    fix: rename user to author

--- apps-api/backoffice/src/modules/feed/index.ts
+++ apps-api/backoffice/src/modules/feed/index.ts
@@ -162,7 +162,7 @@ export const FeedController = new Elysia({
     }
   )
   .get(
-    '/users/:id',
+    '/author/:id',
     async ({ feedService, query, params, status, user }) => {
       const result = await feedService.getFeedByAuthorId(params.id, user?.id, {
         cursor: query?.cursor,
@@ -188,8 +188,8 @@ export const FeedController = new Elysia({
         ),
       },
       detail: {
-        summary: 'Get feed items by user ID',
-        description: 'Fetch feed items created by a specific user',
+        summary: 'Get feed items by author ID',
+        description: 'Fetch feed items created by a specific author',
       },
     }
   )
