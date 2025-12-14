40feada6b915424f2206d9a6cbf43ae4ecfcdd87
miello <partearth1212@gmail.com>
2025-12-14 13:26:26 +0700

    feat: new mini app url with button and name

--- apps-client/mobile/app/(tabs)/(feed,official)/mini-app/[slug]/index.tsx
+++ apps-client/mobile/app/(tabs)/(feed,official)/mini-app/[slug]/index.tsx
@@ -1,12 +1,13 @@
-import { useEffect } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { View } from 'react-native'
 import { WebView } from 'react-native-webview'
 
 import { Button } from '@pple-today/ui/button'
 import { Icon } from '@pple-today/ui/icon'
+import { Text } from '@pple-today/ui/text'
 import Constants from 'expo-constants'
 import { useLocalSearchParams, useRouter } from 'expo-router'
-import { ArrowLeftIcon } from 'lucide-react-native'
+import { ArrowLeftIcon, XIcon } from 'lucide-react-native'
 
 import NotFound from '@app/app/+not-found'
 import LoadingPage from '@app/app/loading'
@@ -18,7 +19,11 @@ const MiniAppWebView = () => {
   const params = useLocalSearchParams()
   const slug = params.slug as string
   const path = params.path as string | undefined
+
   const tokenExchangeMiniAppResult = reactQueryClient.useMutation('post', '/auth/mini-app/:slug')
+
+  const [canGoBack, setCanGoBack] = useState(false)
+  const miniAppRef = useRef<WebView>(null)
 
   useEffect(() => {
     if (!slug) {
@@ -37,14 +42,41 @@ const MiniAppWebView = () => {
 
   return (
     <View className="flex-1 flex-col bg-base-bg-default">
-      <View className="pt-safe-offset-4 pb-2 px-4 bg-base-bg-white">
+      <View className="pt-safe-offset-4 pb-2 px-4 bg-base-bg-white flex-row items-center">
+        {canGoBack ? (
+          <Button
+            variant="ghost"
+            size="icon"
+            className="size-6"
+            onPress={() => {
+              if (miniAppRef.current) {
+                miniAppRef.current.goBack()
+              }
+            }}
+            aria-label="Back to previous web page"
+          >
+            <Icon icon={ArrowLeftIcon} size={24} />
+          </Button>
+        ) : (
+          <View className="size-5" />
+        )}
+        <Text className="flex-1 text-center align-middle">
+          {tokenExchangeMiniAppResult.isSuccess ? tokenExchangeMiniAppResult.data.appName : ''}
+        </Text>
         <Button
-          variant="outline-primary"
+          variant="ghost"
           size="icon"
-          onPress={() => router.back()}
-          aria-label="Go back"
+          className="size-6"
+          aria-label="Close mini app"
+          onPress={() => {
+            if (router.canGoBack()) {
+              router.back()
+            } else {
+              router.dismissTo('/')
+            }
+          }}
         >
-          <Icon icon={ArrowLeftIcon} size={24} />
+          <Icon icon={XIcon} size={24} />
         </Button>
       </View>
       <View className="flex-1">
@@ -54,6 +86,10 @@ const MiniAppWebView = () => {
           <NotFound />
         ) : (
           <WebView
+            ref={miniAppRef}
+            onNavigationStateChange={(navState) => {
+              setCanGoBack(navState.canGoBack)
+            }}
             userAgent={`PPLETodayApp/${Constants.expoConfig?.version ?? 'local'} MiniApp`}
             source={{ uri: tokenExchangeMiniAppResult.data.url }}
             startInLoadingState={true}
