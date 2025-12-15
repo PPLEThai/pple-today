f59369ce54860f76284ed4f04308f68db1baea30
miello <partearth1212@gmail.com>
2025-12-15 17:18:19 +0700

    refactor: separate config to variable

--- apps-api/ballot-crypto/src/plugins/kms.ts
+++ apps-api/ballot-crypto/src/plugins/kms.ts
@@ -26,16 +26,17 @@ export class KeyManagementService {
     },
     private readonly loggerService: ElysiaLoggerInstance
   ) {
-    this.kmsClient = new KeyManagementServiceClient({
-      projectId: config.projectId,
-      credentials:
-        config.clientEmail && config.privateKey
-          ? {
+    const kmsClientConfig =
+      config.projectId && config.clientEmail && config.privateKey
+        ? {
+            projectId: config.projectId,
+            credentials: {
               client_email: config.clientEmail,
               private_key: config.privateKey,
-            }
-          : undefined,
-    })
+            },
+          }
+        : undefined
+    this.kmsClient = new KeyManagementServiceClient(kmsClientConfig)
   }
 
   private getProjectId() {
