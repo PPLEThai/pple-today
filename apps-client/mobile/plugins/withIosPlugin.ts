7096d0ba0d4e70b9fa733c229a195869b7a632d2
miello <partearth1212@gmail.com>
2026-04-21 15:58:08 +0700

    fix: attempt to fix google utilities error

--- apps-client/mobile/plugins/withIosPlugin.ts
+++ apps-client/mobile/plugins/withIosPlugin.ts
@@ -1,6 +1,14 @@
 import { ConfigPlugin, withPodfile } from 'expo/config-plugins'
 const withIosPlugin: ConfigPlugin = (config) => {
   return withPodfile(config, (config) => {
+    // Add pod 'GoogleUtilities' to target 'PPLEToday' in Podfile
+    config.modResults.contents = config.modResults.contents.replaceAll(
+      'use_expo_modules!',
+      `use_expo_modules!
+
+  pod 'GoogleUtilities'`
+    )
+
     config.modResults.contents += `
 target 'NotificationServiceExtension' do
   use_frameworks! :linkage => :static
